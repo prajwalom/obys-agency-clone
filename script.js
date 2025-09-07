fd37de2ae088907a378f745474acbd57ffb8cb60
prajwalome <prajwalpadole99@gmail.com>
2025-09-07 19:50:47 +0530

    loader updated

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ gsap.from(".line h1", {
     duration: 0.6,
     delay: 0.5,
 })
+
+
+//
+
+let h5Timer = document.getElementById("loader-h5")
+let increase = 0
+setInterval(function() {
+    if(increase<100) {
+        h5Timer.innerHTML = increase++
+    } else
+
+    h5Timer.innerHTML = increase
+
+}, 35)
